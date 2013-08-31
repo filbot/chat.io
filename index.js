e16f9fe9a7e06cb2e4381ccb50471e947b60f010
Filip Adamczyk <filip@filbot.com>
2013-08-31 09:46:33 -0700

    Added comments

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-var express = require ("express");
-var app = express ();
-var port = 3700;
+var express = require ("express"); // setup for express app
+var app = express (); // assigning express to "app" variable
+var port = 3700; // assigning port number
 
-app.set('views', __dirname + '/tpl');
-app.set('view engine', "jade");
-app.engine('jade', require('jade').__express);
-app.get("/", function(req, res) {
+app.set('views', __dirname + '/tpl'); // jade views directory
+app.set('view engine', "jade"); // specifying jade as the templating engine
+app.engine('jade', require('jade').__express); // setup for jade templating engine
+app.get("/", function(req, res) { // route setup using render for jade view
     res.render("page");
 });
 
-app.listen(port);
-console.log("Listening on port " + port);
+// socket.io connection handler
+io.socket.on('connection', function (socket) {
+    socket.emit('message', { message: 'welcome to the chat' });
+    socket.on('send', function (data) {
+        io.socket.emit('message', data);
+    });
+});
+
+app.use(express.static(__dirname + '/public')); // ???
+var io = require('socket.io').listen(app.listen(port)); // set port for socket.io
+console.log("Listening on port " + port); // send to stnd out
