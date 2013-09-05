1c725bbe971c364c18bd5bb29e30ff580a96dde9
Filip Adamczyk <filip@filbot.com>
2013-09-04 21:03:35 -0700

    Added line for local dev environment

--- public/chat.js
+++ public/chat.js
@@ -2,6 +2,8 @@ window.onload = function () {
 
     var messages = [];
     var socket = io.connect('http://filbot-chat.jit.su');
+    //for local dev
+    // var socket = io.connect('http://localhost:4000');
     var field = document.getElementById("field");
     var sendButton = document.getElementById("send");
     var content = document.getElementById("content");
